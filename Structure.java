80d18931bf78cd90088f7198626333c7b485d88f
kavik1718003 <kavikamaraj1311@gmail.com>
2025-01-17 21:51:12 +0530

    Upload a java structure

---
+++ Structure.java
@@ -0,0 +1,31 @@
+// Package declaration (optional)
+// package basic;
+
+// Class declaration
+// public class Example {
+    // Variable (data)
+    // int number; // Instance variable
+
+    // Method (logic)
+    // void displayMessage() {
+        // System.out.println("Hello, this is a basic structure of a Java program!");
+    // }
+
+    // Main method
+    // public static void main(String[] args) {
+        // Object creation
+        // Example obj = new Example();
+
+        // Assigning value to the variable
+        // obj.number = 10;
+
+        // Calling the variable
+        // System.out.println("The number is: " + obj.number);
+
+        // Calling the method
+        // obj.displayMessage();
+    // }
+// }
+
+    
+
